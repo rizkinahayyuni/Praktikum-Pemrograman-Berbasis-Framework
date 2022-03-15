e52b792f3ac8d63b84ce89fa0ce6997da10b1a43
rizkinahayyuni <rizkinahayyuni@gmail.com>
2022-03-16 03:22:24 +0700

    Praktikum 2 - URL Parameters

--- Praktikum 5-React Router/router-react/src/App.js
+++ Praktikum 5-React Router/router-react/src/App.js
@@ -3,55 +3,44 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Link
+  Link,
+  useParams
 } from "react-router-dom";
 
-export default function BasicExample() {
+export default function ParamsExample() {
   return (
     <Router>
       <div>
+        <h2>Accounts</h2>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/netflix">Netflix</Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link to="/gmail">Gmail</Link>
           </li>
           <li>
-            <Link to="/dashboard">Dashboard</Link>
+            <Link to="/yahoo">Yahoo</Link>
+          </li>
+          <li>
+            <Link to="/amazon">Amazon</Link>
           </li>
         </ul>
         <hr />
         <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route exact path="/:id" element={<Child />} />
         </Routes>
       </div>
     </Router>
   );
 }
 
-function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  );
-}
+function Child() {
+  let { id } = useParams();
 
-function About() {
   return (
     <div>
-      <h2>About</h2>
-    </div>
-  );
-}
-
-function Dashboard() {
-  return (
-    <div>
-      <h2>Dashboard</h2>
+      <h3>ID : {id}</h3>
     </div>
   );
 }
