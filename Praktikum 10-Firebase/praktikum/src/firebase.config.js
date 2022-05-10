3f1b375425b57cd34ef44023cfb19b814e912efa
rizkinahayyuni <rizkinahayyuni@gmail.com>
2022-05-10 17:45:44 +0700

    praktikum 10

---
+++ Praktikum 10-Firebase/praktikum/src/firebase.config.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyAWiGF0WhHz4Sr2sMA5nMpj1W9KyVWQRMo",
+    authDomain: "pbf-modul10-bfc0b.firebaseapp.com",
+    projectId: "pbf-modul10-bfc0b",
+    storageBucket: "pbf-modul10-bfc0b.appspot.com",
+    messagingSenderId: "680255834281",
+    appId: "1:680255834281:web:074668e9d044a9edc541b8"
+};
