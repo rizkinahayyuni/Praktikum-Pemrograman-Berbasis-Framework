a5e25ff81ca790bcbd763362fa5606b574bfa92f
rizkinahayyuni <rizkinahayyuni@gmail.com>
2022-05-31 07:31:20 +0700

    praktikum

---
+++ Praktikum 12-CRUD/firebase-crud/src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA9v0y6x6H3l0ki6Qljkk3g_E7arlUQXFE",
+  authDomain: "modul11-pbf-298e8.firebaseapp.com",
+  databaseURL: "https://modul11-pbf-298e8-default-rtdb.firebaseio.com",
+  projectId: "modul11-pbf-298e8",
+  storageBucket: "modul11-pbf-298e8.appspot.com",
+  messagingSenderId: "1071807158544",
+  appId: "1:1071807158544:web:d2aab6f861cdd57c7b2d95",
+  measurementId: "G-X12DW725W4"
+};
+
+export default firebaseConfig;
